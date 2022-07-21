ac13043ea851224dcea2b69c6431b482b5fb434c
bedantH <mohanesh.h2003@gmail.com>
2022-07-21 20:25:28 +0530

    made some changes

--- styles/components/quantityStepper.styles.js
+++ styles/components/quantityStepper.styles.js
@@ -1,10 +1,8 @@
-import { textAlign } from '@mui/system';
 import { StyleSheet, Text, View } from 'react-native';
 
 export const styles = StyleSheet.create({
     stepperContainer: {
         flexDirection: 'row',
-        width: 'fit-content',
         backgroundColor: "#eaeaea",
     },
     stepper: {

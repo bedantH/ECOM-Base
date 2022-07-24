68b24026757c608fc33fd46f0f276e35bf5002c1
bedantH <mohanesh.h2003@gmail.com>
2022-07-24 13:25:09 +0530

    sms integration done

--- services/message.js
+++ services/message.js
@@ -1,3 +1,5 @@
 import axios from "axios";
 
 export const sendWhatsAppMessage = (body) => axios.post("https://connect.pabbly.com/workflow/sendwebhookdata/IjQ2NDM2MSI_3D", { ...body }) 
+
+export const sendSMS = (body) => axios.post("https://peaceful-chamber-47811.herokuapp.com/", {...body})
